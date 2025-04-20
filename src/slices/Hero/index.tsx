dbc58048a9ccbb560e287837b23923d966533f97
Diego <diegoooliveira237@gmail.com>
2025-04-20 14:00:25 -0300

    :sparkles: feat:  configure prismic slices

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,119 @@
+"use client";
+import { FC } from "react";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { asText, Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+
+import { TextSplitter } from "@/components/TextSplitter";
+import { Bounded } from "@/components/Boundede";
+import Button from "@/components/Button";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+
+const Hero: FC<HeroProps> = ({ slice }) => {
+  useGSAP(() => {
+    const introTl = gsap.timeline();
+
+    introTl
+      .set(".hero", { opacity: 1 })
+      .from(".hero-header-word", {
+        scale: 3,
+        opacity: 0,
+        ease: "power4.in",
+        delay: 0.3,
+        stagger: 1,
+      })
+      .from(".hero-subheading", { opacity: 0, y: 30 }, "+=.8")
+      .from(".hero-body", {
+        opacity: 0,
+        y: 10,
+      })
+      .from(".hero-button", {
+        opacity: 0,
+        y: 10,
+        duration: 0.6,
+      });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "hero",
+        start: "top  top",
+        end: "bottom bottom",
+        scrub: 1.5,
+        // markers: true,
+      },
+    });
+
+    scrollTl.fromTo(
+      "body",
+      {
+        backgroundColor: "#292524",
+      },
+      { backgroundColor: "#a8a29e", overwrite: "auto" },
+      1,
+    );
+  });
+
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="hero opacity-0"
+    >
+      <div className="grid">
+        <div className="grid h-screen place-items-center">
+          <div className="grid auto-rows-min place-items-center text-center">
+            <h1 className="hero-header text-7xl font-black uppercase leading-[.8] text-stone-400 md:text-[9rem] lg:text-[13rem]">
+              <TextSplitter
+                text={asText(slice.primary.heding)}
+                wordDisplayStyle="block"
+                className="hero-header-word"
+              />
+            </h1>
+            <div className="hero-subheading mt-12 text-5xl font-semibold text-sky-900 lg:text-6xl">
+              <PrismicRichText field={slice.primary.subheading} />
+            </div>
+            <div className="hero-body text-2xl font-normal text-sky-900">
+              <PrismicRichText field={slice.primary.body} />
+            </div>
+            <Button
+              buttonLink={slice.primary.button_link}
+              buttonText={slice.primary.button_text}
+              className="hero-button mt-12"
+            />
+          </div>
+        </div>
+        <div className="text-side relative z-[80] grid h-screen items-center gap-4 md:grid-cols-2">
+          <PrismicNextImage
+            className="w-full md:hidden"
+            field={slice.primary.cans_image}
+          />
+          <div>
+            <h2 className="text-side-heading text-balance text-6xl font-black uppercase text-sky-900 lg:text-8xl">
+              <TextSplitter text={asText(slice.primary.second_heading)} />
+            </h2>
+            <div className="text-side-body max-w-x1 text-x1 mt-4 text-balance font-normal text-sky-900">
+              <PrismicRichText field={slice.primary.second_body} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Bounded>
+  );
+};
+
+export default Hero;
